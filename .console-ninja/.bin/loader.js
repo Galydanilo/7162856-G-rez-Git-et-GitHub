5c99864a448d97d8f666caf4ea32b6b70bc73095
Galy Danilo <140662974+Galydanilo@users.noreply.github.com>
2023-07-28 23:13:16 +0200

    add html file

---
+++ .console-ninja/.bin/loader.js
@@ -0,0 +1,1 @@
+"use strict";try{let{createRequire:e}=require("module");e("file:///c:/Users/MrGal/.vscode/extensions/wallabyjs.console-ninja-1.0.351/out/buildHook/")("./index.js").default({tool:"node"})}catch(e){console.log(e)}
